2d1d1087fe9efe1eabd51ed59b9c03c5567e2f5b
VictorHYu <victor.yu.canada@gmail.com>
2018-11-20 02:53:40 -0500

    foreman

--- PMAS/app.js
+++ PMAS/app.js
@@ -52,9 +52,4 @@ app.get('/musicmetadata', function (req, res) {
     });
 });
 
-app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-});
-
 module.exports = app;
